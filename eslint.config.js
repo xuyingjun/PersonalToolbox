b762f9c0f684ba636ec8663a8df8535deaa80950
autoreport <16405047+autoreport@user.noreply.gitee.com>
2026-08-29 07:44:34 +0800

    fix JSX lint variable detection

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -9,12 +10,14 @@ export default defineConfig([
   {
     files: ['**/*.{js,jsx}'],
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'react/jsx-uses-vars': 'error',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
     languageOptions: {

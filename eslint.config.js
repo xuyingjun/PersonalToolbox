4316d3c0d553ad575ea11eb12e93100c4759f442
autoreport <16405047+autoreport@user.noreply.gitee.com>
2026-08-29 07:42:48 +0800

    fix eslint config compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,15 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
-    extends: [js.configs.recommended, reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
     languageOptions: {
       ecmaVersion: 2022,
       globals: globals.browser,
